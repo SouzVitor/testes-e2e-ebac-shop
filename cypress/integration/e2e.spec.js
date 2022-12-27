cea9e232bc0005dd16e09da9af617a0847785e63
SouzVitor <vitortecam@gmail.com>
2022-12-26 21:47:56 -0400

    alterações da tarefa 12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -11,50 +11,75 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/')
-    });
+        describe('Funcionalidade Página de produtos', () => {
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.get('[class="product-block grid"]')
-            .first()
-            .click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get(':nth-child(1) > .item > .products-grid > .product-block').click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get(':nth-child(2) > .item > .products-grid > .product-block').click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get(':nth-child(3) > .item > .products-grid > .product-block').click()
-        cy.get('.single_add_to_cart_button').click()       
-        cy.get('.woocommerce-message > .button').click() 
-        cy.get('.checkout-button').click()
-               
-
-        let nomefaker = faker.name.firstName()
-        let sobrenomefaker = faker.name.lastName()
-        let endereco = faker.address.city()
-        let empresa = faker.company.companyName()
-        let cidade = faker.address.cityName()
-        let cep = faker.address.zipCode()
-        let telefone = faker.phone.phoneNumber()
-        let emailfaker = faker.internet.email(nomefaker)
-
-        cy.get('#billing_first_name').type(nomefaker)
-        cy.get('#billing_last_name').type(sobrenomefaker)
-        cy.get('#billing_company').type(empresa)
-        cy.get('#billing_address_1').type(endereco)
-        cy.get('#billing_city').type(cidade)  
-        cy.get('#billing_postcode').type(cep) 
-        cy.get('#billing_phone').type(telefone)
-        cy.get('#billing_email').type(emailfaker) 
+            before(() => {
+                cy.visit('produtos/')
         
-        cy.get('#payment_method_cod').click()
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
+            });
+        
+            it('Deve selecionar um produto da lista', () => {
+                cy.get('[class="product-block grid"]')
+                    .first()
+                    .click()
+        
+                cy.get('[class="product-block grid"]')
+                    .last()
+                    .click()
+        
+                cy.get('[class="product-block grid"]')
+                    .eq(2)
+                    .click()
+                
+                cy.visit('produtos/page/2/')
+                cy.get('[class="product-block grid"]')
+                .contains('Autumn Pullie')
+                .click()
+        
+            });
+        
+            it('Deve adicionar um produto ao carrinho', () => {
+                var quantidade = 5
+                
+                cy.visit('produtos/page/2/')
+                cy.get('[class="product-block grid"]')
+                .contains('Autumn Pullie').click()
+                cy.get('.button-variable-item-S').click()
+                cy.get('.button-variable-item-Red').click()
+                cy.get('.input-text').clear().type(quantidade)
+                cy.get('.single_add_to_cart_button').click()
+        
+                cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+                cy.get('.woocommerce-message').should('contain', quantidade + ' × “Autumn Pullie” foram adicionados no seu carrinho.')
+                cy.get('.woocommerce-message > .button').click()
+                cy.get('.checkout-button').click()
+                
+        
+                    let nomefaker = faker.name.firstName()
+                    let sobrenomefaker = faker.name.lastName()
+                    let endereco = faker.address.city()
+                    let empresa = faker.company.companyName()
+                    let cidade = faker.address.cityName()
+                    let cep = faker.address.zipCode()
+                    let telefone = faker.phone.phoneNumber()
+                    let emailfaker = faker.internet.email(nomefaker)
+        
+                    cy.get('#billing_first_name').type(nomefaker)
+                    cy.get('#billing_last_name').type(sobrenomefaker)
+                    cy.get('#billing_company').type(empresa)
+                    cy.get('#billing_address_1').type(endereco)
+                    cy.get('#billing_city').type(cidade)  
+                    cy.get('#billing_postcode').type(cep) 
+                    cy.get('#billing_phone').type(telefone)
+                    cy.get('#billing_email').type(emailfaker) 
+                    
+                    cy.get('#payment_method_cod').click()
+                    cy.get('#terms').click()
+                    cy.get('#place_order').click()
+        
+                });   
+                          
             
-
+        
+        });
     });
-
-  
-
-});
